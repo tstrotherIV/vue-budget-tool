44e90d6b311b7e15ecaade1406153cc3a71645da
Tom Strother <tom_strother@yahoo.com>
2021-03-10 16:49:41 -0600

    m

--- src/components/monthlyExpenseBreakdown.vue
+++ src/components/monthlyExpenseBreakdown.vue
@@ -1,12 +1,8 @@
 <template>
-  <v-container>
-    <v-card height="100%">
-      <v-card-title class="purple--text"
-        >Monthly Expense Breakdown</v-card-title
-      >
-      <apexchart type="pie" :options="options" :series="series" height="300" />
-    </v-card>
-  </v-container>
+  <v-card height="100%">
+    <v-card-title class="purple--text">Monthly Expense Breakdown</v-card-title>
+    <apexchart type="pie" :options="options" :series="series" height="300" />
+  </v-card>
 </template>
 
 <script>
@@ -14,14 +10,14 @@ export default {
   props: ["expensesList", "annualIncomeAmount"],
   computed: {
     series() {
-      return this.expensesList.map(e => parseInt(e.expense_amount));
+      return this.expensesList.map((e) => parseInt(e.expense_amount));
     },
     options() {
       return {
-        labels: this.expensesList.map(e => e.expense_name)
+        labels: this.expensesList.map((e) => e.expense_name),
       };
-    }
-  }
+    },
+  },
 };
 </script>
 
